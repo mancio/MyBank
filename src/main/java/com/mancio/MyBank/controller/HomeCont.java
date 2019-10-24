c6253f1727bacfae31ee7c864e3daf85d914a751
andrea.mancini <mancioman2@gmail.com>
2019-10-24 14:53:13 +0200

    new

--- src/main/java/com/mancio/MyBank/controller/HomeCont.java
+++ src/main/java/com/mancio/MyBank/controller/HomeCont.java
@@ -1,6 +1,10 @@
 package com.mancio.MyBank.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
 
 public class HomeCont {
 
@@ -12,5 +16,15 @@ public class HomeCont {
     @RequestMapping("/login")
     public String login(){
         return "login page";
+    }
+
+    @RequestMapping("/signup")
+    public String signup(){
+        return "signup page";
+    }
+
+    @RequestMapping(value = "/privatearea", method = RequestMethod.GET)
+    public String privatepage(Model model, Principal principal){
+        
     }
 }
