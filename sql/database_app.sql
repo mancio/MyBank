df4878d7bd113031607c122469b4d7ec3cf682a0
andrea.mancini <mancioman2@gmail.com>
2019-10-28 13:31:00 +0100

    new

--- sql/database_app.sql
+++ sql/database_app.sql
@@ -1,17 +1,4 @@
-/*
-    DROP TABLE IF EXISTS `bank`;
-    DROP TABLE IF EXISTS `branches`;
-    DROP TABLE IF EXISTS `branch_services`;
-    DROP TABLE IF EXISTS `user_addresses`;
-    DROP TABLE IF EXISTS `branch_addresses`;
-    DROP TABLE IF EXISTS `user`;
-    DROP TABLE IF EXISTS `bank_account`;
-    DROP TABLE IF EXISTS `transaction`;
-    DROP TABLE IF EXISTS `user_role`;
-    DROP TABLE IF EXISTS `role`;
-*/
-
-# drop all tables independently from foreign keys
+# drop all tables
 SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS
 bank, branches, branch_services, user_addresses, branch_addresses,
@@ -24,28 +11,9 @@ CREATE TABLE IF NOT EXISTS `bank` (
   `bank_details` varchar(255)
 );
 
-
-CREATE TABLE IF NOT EXISTS `branches` (
-  `branch_ID` bigint PRIMARY KEY,
-  `b_address_ID` bigint,
-  `bank_ID` bigint,
-  `branch_type_code` varchar(50),
-  `branch_details` varchar(255)
-);
-
 CREATE TABLE IF NOT EXISTS `branch_services` (
   `branch_type_code` varchar(50) PRIMARY KEY,
   `service_list` varchar(255)
-);
-
-CREATE TABLE IF NOT EXISTS `user_addresses` (
-  `u_address_ID` bigint PRIMARY KEY,
-  `street` varchar(60) NOT NULL,
-  `city` varchar(60),
-  `zip` varchar(60),
-  `region` varchar(60),
-  `country` varchar(60),
-  `state` varchar(60)
 );
 
 CREATE TABLE IF NOT EXISTS `branch_addresses` (
@@ -57,6 +25,29 @@ CREATE TABLE IF NOT EXISTS `branch_addresses` (
   `country` varchar(60),
   `state` varchar(60)
 );
+
+CREATE TABLE IF NOT EXISTS `branches` (
+  `branch_ID` bigint PRIMARY KEY,
+  `b_address_ID` bigint,
+  `bank_ID` bigint,
+  `branch_type_code` varchar(50),
+  `branch_details` varchar(255),
+  FOREIGN KEY (bank_ID) REFERENCES bank(bank_ID),
+  FOREIGN KEY (branch_type_code) REFERENCES branch_services(branch_type_code),
+  FOREIGN KEY (b_address_ID) REFERENCES branch_addresses(b_address_ID)
+);
+
+
+CREATE TABLE IF NOT EXISTS `user_addresses` (
+  `u_address_ID` bigint PRIMARY KEY,
+  `street` varchar(60) NOT NULL,
+  `city` varchar(60),
+  `zip` varchar(60),
+  `region` varchar(60),
+  `country` varchar(60),
+  `state` varchar(60)
+);
+
 
 CREATE TABLE IF NOT EXISTS `user` (
   `user_ID` bigint PRIMARY KEY,
@@ -70,14 +61,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `phone` int(20) UNIQUE,
   `mail` varchar(20) UNIQUE,
   `u_address_ID` bigint,
-  `branch_ID` bigint
+  `branch_ID` bigint,
+  FOREIGN KEY (branch_ID) REFERENCES branches(branch_ID),
+  FOREIGN KEY (u_address_ID) REFERENCES user_addresses(u_address_ID)
 );
+
 
 CREATE TABLE IF NOT EXISTS `bank_account` (
   `account_number` int(60) PRIMARY KEY,
   `country_code` varchar(5),
   `user_ID` bigint,
-  `balance` decimal(20,2)
+  `balance` decimal(20,2),
+  FOREIGN KEY (user_ID) REFERENCES user(user_ID)
 );
 
 CREATE TABLE IF NOT EXISTS `transaction` (
@@ -88,13 +83,8 @@ CREATE TABLE IF NOT EXISTS `transaction` (
   `receiver_account_number` varchar(60),
   `trans_amount` decimal(20,2) COMMENT 'do not use float',
   `trans_description` varchar(255),
-  `trans_status` varchar(255)
-);
-
-CREATE TABLE IF NOT EXISTS `user_role` (
-  `user_role_ID` bigint PRIMARY KEY,
-  `user_ID` bigint,
-  `role_ID` bigint
+  `trans_status` varchar(255),
+  FOREIGN KEY (sender_account_number) REFERENCES bank_account(account_number)
 );
 
 CREATE TABLE IF NOT EXISTS `role` (
@@ -102,4 +92,11 @@ CREATE TABLE IF NOT EXISTS `role` (
   `role_name` varchar(255)
 );
 
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `user_role_ID` bigint PRIMARY KEY,
+  `user_ID` bigint,
+  `role_ID` bigint,
+  FOREIGN KEY (user_ID) REFERENCES user(user_ID),
+  FOREIGN KEY (role_ID) REFERENCES role(role_ID)
+);
 
