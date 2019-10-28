aef4a08fe23f10b1f7f2210f39e93cc08248f820
andrea.mancini <mancioman2@gmail.com>
2019-10-28 11:19:03 +0100

    new

--- sql/database_app.sql
+++ sql/database_app.sql
@@ -1,82 +1,104 @@
-CREATE TABLE IF NOT EXIST `bank` (
-  `bank_ID` int PRIMARY KEY,
+/*
+    DROP TABLE IF EXISTS `bank`;
+    DROP TABLE IF EXISTS `branches`;
+    DROP TABLE IF EXISTS `branch_services`;
+    DROP TABLE IF EXISTS `user_addresses`;
+    DROP TABLE IF EXISTS `branch_addresses`;
+    DROP TABLE IF EXISTS `user`;
+    DROP TABLE IF EXISTS `bank_account`;
+    DROP TABLE IF EXISTS `transaction`;
+    DROP TABLE IF EXISTS `user_role`;
+    DROP TABLE IF EXISTS `role`;
+*/
+
+# drop all tables independently from foreign keys
+SET foreign_key_checks = 0;
+DROP TABLE IF EXISTS
+bank, branches, branch_services, user_addresses, branch_addresses,
+user, bank_account, transaction, user_role, role;
+SET foreign_key_checks = 1;
+
+
+CREATE TABLE IF NOT EXISTS `bank` (
+  `bank_ID` bigint PRIMARY KEY,
   `bank_details` varchar(255)
 );
 
-CREATE TABLE IF NOT EXIST `branches` (
-  `branch_ID` int PRIMARY KEY,
-  `b_address_ID` int,
-  `bank_ID` int,
-  `branch_type_code` varchar(10),
+
+CREATE TABLE IF NOT EXISTS `branches` (
+  `branch_ID` bigint PRIMARY KEY,
+  `b_address_ID` bigint,
+  `bank_ID` bigint,
+  `branch_type_code` varchar(50),
   `branch_details` varchar(255)
 );
 
-CREATE TABLE IF NOT EXIST `branch_services` (
-  `branch_type_code` varchar(10) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS `branch_services` (
+  `branch_type_code` varchar(50) PRIMARY KEY,
   `service_list` varchar(255)
 );
 
-CREATE TABLE IF NOT EXIST `user_addresses` (
-  `u_address_ID` int PRIMARY KEY,
-  `street` varchar(255),
-  `city` varchar(255),
-  `zip` varchar(255),
-  `region` varchar(255),
-  `country` varchar(255),
-  `state` varchar(255)
+CREATE TABLE IF NOT EXISTS `user_addresses` (
+  `u_address_ID` bigint PRIMARY KEY,
+  `street` varchar(60) NOT NULL,
+  `city` varchar(60),
+  `zip` varchar(60),
+  `region` varchar(60),
+  `country` varchar(60),
+  `state` varchar(60)
 );
 
-CREATE TABLE IF NOT EXIST `branch_addresses` (
-  `b_address_ID` int PRIMARY KEY,
-  `street` varchar(255),
-  `city` varchar(255),
-  `zip` varchar(255),
-  `region` varchar(255),
-  `country` varchar(255),
-  `state` varchar(255)
+CREATE TABLE IF NOT EXISTS `branch_addresses` (
+  `b_address_ID` bigint PRIMARY KEY,
+  `street` varchar(60),
+  `city` varchar(60),
+  `zip` varchar(60),
+  `region` varchar(60),
+  `country` varchar(60),
+  `state` varchar(60)
 );
 
-CREATE TABLE IF NOT EXIST `user` (
-  `user_ID` int PRIMARY KEY,
-  `username` varchar(10) UNIQUE,
-  `password` varchar(50),
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_ID` bigint PRIMARY KEY,
+  `username` varchar(60) UNIQUE,
+  `password` varchar(60),
   `access_status_code` tinyint(1) COMMENT '1 ok, 0 banned',
-  `name` varchar(20),
-  `middle_name` varchar(20),
-  `last_name` varchar(20),
+  `name` varchar(60),
+  `middle_name` varchar(60),
+  `last_name` varchar(60),
   `phone_prefix` int(5),
   `phone` int(20) UNIQUE,
   `mail` varchar(20) UNIQUE,
-  `u_address_ID` int,
-  `branch_ID` int
+  `u_address_ID` bigint,
+  `branch_ID` bigint
 );
 
-CREATE TABLE IF NOT EXIST `bank_account` (
-  `account_number` int(50) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS `bank_account` (
+  `account_number` int(60) PRIMARY KEY,
   `country_code` varchar(5),
-  `user_ID` int,
-  `balance` decimal(50)
+  `user_ID` bigint,
+  `balance` decimal(20,2)
 );
 
-CREATE TABLE IF NOT EXIST `transaction` (
-  `transaction_ID` int PRIMARY KEY,
-  `sender_account_number` int(50),
+CREATE TABLE IF NOT EXISTS `transaction` (
+  `transaction_ID` bigint PRIMARY KEY,
+  `sender_account_number` int(60),
   `transfer_date` Date,
   `receiver_name` varchar(255),
-  `receiver_account_number` varchar(50),
-  `trans_amount` decimal(50) COMMENT 'do not use float',
+  `receiver_account_number` varchar(60),
+  `trans_amount` decimal(20,2) COMMENT 'do not use float',
   `trans_description` varchar(255),
   `trans_status` varchar(255)
 );
 
-CREATE TABLE IF NOT EXIST `user_role` (
-  `user_role_ID` int PRIMARY KEY,
-  `user_ID` int,
-  `role_ID` varchar(255)
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `user_role_ID` bigint PRIMARY KEY,
+  `user_ID` bigint,
+  `role_ID` bigint
 );
 
-CREATE TABLE IF NOT EXIST `role` (
-  `role_ID` int PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS `role` (
+  `role_ID` bigint PRIMARY KEY,
   `role_name` varchar(255)
 );
 
